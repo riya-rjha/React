01fdebf79ab987271ba36a131bfc2df0115bc54a
riya-rjha <riyaranjanjha2006@gmail.com>
2023-11-15 15:28:36 +0530

    Setting up React Version 6 Routers

--- React Router/src/App.jsx
+++ React Router/src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import { useState, useEffect } from 'react';
+import { Route, Routes, useNavigate} from 'react-router-dom' //React Router Version 6
+import Header from "./Header";
+import Nav from "./Nav";
+import Home from "./Home";
+import NewPost from "./NewPost";
+import PostPage from "./PostPage";
+import About from "./About";
+import Missing from "./Missing";
+import Footer from "./Footer";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      <Header/>
+      <Nav/>
+      <Home/>
+      <NewPost/>
+      <PostPage/>
+      <About/>
+      <Missing/>
+      <Footer/>
+    </div>
+  );
 }
 
 export default App
