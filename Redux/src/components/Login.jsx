e9166df27bfe6d3f8e3afb281fc0bbcc8d5d0f0c
riya-rjha <riyaranjanjha2006@gmail.com>
2024-04-04 02:20:36 +0530

    useSelector in Redux

---
+++ Redux/src/components/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div className='login-form'>
+        <button>Login</button>
+    </div>
+  )
+}
+
+export default Login
---
+++ Redux/src/components/Profile.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
+
+function Profile() {
+    const user = useSelector((state) => state.user.value);
+
+    const [name, setName] = useState("");
+    const [age, setAge] = useState("");
+    const [email, setEmail] = useState("");
+
+    return (
+        <div className="login-form">
+            <label htmlFor="name">Name: {user.name}</label>
+            <input type="text" value={name} onChange={(e)=>setName(e.target.value)}/>
+            <label htmlFor="name">Age {user.age}</label>
+            <input type="number" value={age} onChange={(e) => setAge(e.target.value)} />
+            <label htmlFor="email">Email: {user.email}</label>
+            <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        </div>
+    )
+}
+
+export default Profile
